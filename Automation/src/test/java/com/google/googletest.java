37c3d6b1cfb1e56b4128ce36fec438e4efdaf908
daniel.hernandez.bar <daniel.hernandez.bar@DESKTOP-M71URN9>
2023-09-05 13:39:48 -0600

    Val

--- Automation/src/test/java/com/google/googletest.java
+++ Automation/src/test/java/com/google/googletest.java
@@ -1,12 +1,20 @@
 package com.google;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class googletest {
   @Test
   public void f() {
+	  
+	  System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chr/chromedriver.exe");
+	  WebDriver driver = new ChromeDriver ();
+	  driver.get("https://www.google.com/");
+	  driver.manage().window().maximize();
+	
   }
   @BeforeTest
   public void beforeTest() {
